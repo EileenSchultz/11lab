923706daa87e4133361a9430012610f01fe09cbe
NilanSeon <148245767+EileenSchultz@users.noreply.github.com>
2026-05-16 19:25:31 +0300

    функционал для сущности Note

---
+++ labrab11/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using labrab11;
+
+Console.WriteLine("Hello, World!");
+await using var db = new DataContext();
+await db.Database.EnsureCreatedAsync();
